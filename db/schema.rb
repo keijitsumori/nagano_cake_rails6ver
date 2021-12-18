212eb342a18a4d1a46a2c8b6fead019b043d31b4
EC2 Default User <ec2-user@ip-172-31-34-180.ec2.internal>
2021-12-18 06:26:45 +0000

    [model] make addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_060130) do
+ActiveRecord::Schema.define(version: 2021_12_18_062159) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
