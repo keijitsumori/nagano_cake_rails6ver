01140a2cc66547631eb5b70a5cb28875a6061d94
EC2 Default User <ec2-user@ip-172-31-34-180.ec2.internal>
2021-12-18 15:59:04 +0000

    [routes] make all routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,29 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
+  
+  namespace :admin do
+    get 'homes' => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    get 'orders/:id' => 'orders#show'
+    patch 'orders/:id' => 'orders#update'
+    patch 'order_details/:id' => 'order_details#update'
+  end
+  
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :customers, only: [:show, :edit, :update]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    post 'orders/comfirm' => 'orders#comfirm'
+    get 'orders/thanks' => 'orders#thanks'
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
 end
